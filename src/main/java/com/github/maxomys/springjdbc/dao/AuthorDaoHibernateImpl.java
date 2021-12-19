69c97e2c122db70b9f6434761698c011f75cf57d
Zaphod <mistrz7losos@live.com>
2021-12-19 03:21:57 +0100

    Add hibernate impl for author dao

---
+++ src/main/java/com/github/maxomys/springjdbc/dao/AuthorDaoHibernateImpl.java
@@ -0,0 +1,53 @@
+package com.github.maxomys.springjdbc.dao;
+
+import com.github.maxomys.springjdbc.domain.Author;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+
+@Component
+public class AuthorDaoHibernateImpl implements AuthorDao {
+
+    private final EntityManagerFactory emf;
+
+    public AuthorDaoHibernateImpl(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public Author getById(Long id) {
+        return getEntityManager().find(Author.class, id);
+    }
+
+    @Override
+    public Author findAuthorByName(String firstName, String lastName) {
+        TypedQuery<Author> query = getEntityManager().createQuery("SELECT a FROM Author a " +
+                "WHERE a.firstName = :first_name AND a.lastName = :last_name", Author.class);
+        query.setParameter("first_name", firstName);
+        query.setParameter("last_name", lastName);
+
+        return query.getSingleResult();
+    }
+
+    @Override
+    public Author saveNewAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public Author updateAuthor(Author author) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthorById(Long id) {
+
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+}
---
+++ src/test/java/com/github/maxomys/springjdbc/dao/AuthorDaoHibernateImplTest.java
@@ -0,0 +1,29 @@
+package com.github.maxomys.springjdbc.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthorDaoHibernateImplTest {
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void findAuthorByName() {
+    }
+
+    @Test
+    void saveNewAuthor() {
+    }
+
+    @Test
+    void updateAuthor() {
+    }
+
+    @Test
+    void deleteAuthorById() {
+    }
+
+}
