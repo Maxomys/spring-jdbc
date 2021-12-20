7f9d7f9e2bd994e1826a6dbac2261df7e3195c5c
Zaphod <mistrz7losos@live.com>
2021-12-20 23:57:13 +0100

    t

--- src/main/java/com/github/maxomys/springjdbc/dao/AuthorDaoHibernateImpl.java
+++ src/main/java/com/github/maxomys/springjdbc/dao/AuthorDaoHibernateImpl.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
